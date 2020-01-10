680e7fdfb4a8b63b70ee05bf72fde0485b610e7f
Inshal Ayaz <lalaayaz@gmail.com>
2020-01-10 11:31:01 +0500

    new delete function

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ let ourForm = document.getElementById("ourForm");
 let newInput = document.getElementById("newInput");
 let to_do_list = document.getElementById("todo-list")
 
+
 ourForm.addEventListener('submit',(e) => {
     e.preventDefault();
     newContent(newInput.value);  
 })
 
 function newContent(x){
-    let ourHtml = `<li> ${x} <button onClick = 'delFunction'>delete</button> </li>`
+    let ourHtml = `<li> ${x} <button onClick = 'deleteItem(this)'>delete</button> </li>`
     to_do_list.insertAdjacentHTML('beforeend', ourHtml);
     newInput.value = "";
     newInput.focus();
+}
+
+function deleteItem(x){
+    x.parentElement.remove();
 }
