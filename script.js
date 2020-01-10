98cef9684e0c801b98350eb982b53d3baac738a3
Inshal Ayaz <lalaayaz@gmail.com>
2020-01-10 11:14:34 +0500

    modified

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let ourForm = document.getElementById("ourForm");
+let newInput = document.getElementById("newInput");
+let to_do_list = document.getElementById("todo-list")
 
 ourForm.addEventListener('submit',(e) => {
-    e.preventDefault;
+    e.preventDefault();
+    newContent(newInput.value);  
 })
+
+function newContent(x){
+    to_do_list.insertAdjacentHTML('beforeend',+ '<li> $(x) </li>' + '<br>');
+}
